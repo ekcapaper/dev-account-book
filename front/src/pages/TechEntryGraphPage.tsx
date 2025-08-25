7e4be80ea63e8fab2808906e83590fd2e8013f9f
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2025-08-25 21:32:44 +0900

    commit

---
+++ front/src/pages/TechEntryGraphPage.tsx
@@ -0,0 +1,11 @@
+import TechGraph from "../components/TechGraph.tsx";
+
+function TechEntrySheetPage() {
+    return (
+        <div>
+            <h1>Tech Graph 페이지</h1>
+            <TechGraph/>
+        </div>
+    );
+}
+export default TechEntrySheetPage;
--- front/src/pages/TechEntrySheetPage.tsx
+++ front/src/pages/TechEntrySheetPage.tsx
@@ -1,12 +1,10 @@
 import TechSheet from "../components/TechSheet.tsx";
-import TechGraph from "../components/TechGraph.tsx";
 
 function TechEntrySheetPage() {
     return (
         <div>
             <h1>Tech Sheet 페이지</h1>
             <TechSheet/>
-            <TechGraph/>
         </div>
     );
 }
