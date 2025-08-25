c68006460ac72e2c6608392f71d0b3e9acd7cdcf
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2025-08-25 22:22:34 +0900

    Update TechEntryGraphPage.tsx

--- front/src/pages/TechEntryGraphPage.tsx
+++ front/src/pages/TechEntryGraphPage.tsx
@@ -1,10 +1,17 @@
 import TechGraph from "../components/TechGraph.tsx";
+import TechSelectTable from "../components/TechSelectTable.tsx";
+import {Space} from "antd";
 
 function TechEntrySheetPage() {
     return (
         <div>
             <h1>Tech Graph 페이지</h1>
-            <TechGraph/>
+            <Space direction={"vertical"}>
+                <Space direction={"horizontal"}>
+                    <TechSelectTable></TechSelectTable>
+                    <TechGraph/>
+                </Space>
+            </Space>
         </div>
     );
 }
