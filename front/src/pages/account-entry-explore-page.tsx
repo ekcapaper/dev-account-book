3d37993d887763432b5f30a0aa15fe4147d2826d
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2025-08-30 23:01:32 +0900

    Update account-entry-explore-page.tsx

--- front/src/pages/account-entry-explore-page.tsx
+++ front/src/pages/account-entry-explore-page.tsx
@@ -1,10 +1,14 @@
 import AccountEntryExplorerTable from "../components/account-entry-explorer-table.tsx";
+import AccountEntryExplorerTableReverse from "../components/account-entry-explorer-table-reverse.tsx";
 
 function TechEntrySheetPage() {
     return (
         <div>
             <h1>Tech Graph 페이지</h1>
+            <h2>정방향 트리</h2>
             <AccountEntryExplorerTable/>
+            <h2>역방향 트리</h2>
+            <AccountEntryExplorerTableReverse/>
         </div>
     );
 }
