00926a22a8f447cbef6b76cc6f07667b2671087c
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2025-08-30 15:53:21 +0900

    Update api.ts

--- front/src/features/accountentry/api.ts
+++ front/src/features/accountentry/api.ts
@@ -116,6 +116,6 @@ export async function deleteAccountEntryRelationshipApi(from_id: string, to_id: 
 }
 
 export async function explorerAccountEntryStartLeaf() {
-    const start_id = "3c2d3b6a-48c7-4ab9-a99e-417e3f3fb7a4";
+    const start_id = "557c8810-edbf-46ce-86ac-85c306ac75a6";
     return http<AccountEntryTree>(`http://127.0.0.1:8000/v1/account-entries/${start_id}/explore-start-leaf`);
 }
