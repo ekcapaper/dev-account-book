e32297b22e210c577d7935532ec5b24672509128
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2025-08-30 16:40:19 +0900

    Update eslint.config.js

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -3,21 +3,29 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import {globalIgnores} from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        rules: {
+            // 선언했는데 안 쓰는 변수는 "경고"만 띄우도록
+            "@typescript-eslint/no-unused-vars": [
+                "off",
+                {"argsIgnorePattern": "^_", "varsIgnorePattern": "^_"}
+            ]
+        }
+
     },
-  },
 ])
