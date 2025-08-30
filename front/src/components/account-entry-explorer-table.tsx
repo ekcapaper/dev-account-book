aa4642c3b87383bb9f60771361bcd5f46ea0830d
ekcapaper <92367474+ekcapaper@users.noreply.github.com>
2025-08-30 16:40:51 +0900

    Update account-entry-explorer-table.tsx

--- front/src/components/account-entry-explorer-table.tsx
+++ front/src/components/account-entry-explorer-table.tsx
@@ -1,11 +1,9 @@
-import React, {useState} from 'react';
-import type {TableColumnsType, TableProps} from 'antd';
+import React from 'react';
+import type {TableColumnsType} from 'antd';
 import {Table} from 'antd';
 import {accountEntryKeys} from "../hooks/query-keys.ts";
 import {useQuery} from "@tanstack/react-query";
 import {explorerAccountEntryStartLeaf} from "../services/account-entry-api-facade.ts";
-
-type TableRowSelection<T extends object = object> = TableProps<T>['rowSelection'];
 
 interface DataType {
     key: React.ReactNode;
@@ -33,95 +31,8 @@ const columns: TableColumnsType<DataType> = [
     }
 ];
 
-const data: DataType[] = [
-    {
-        key: 1,
-        name: 'John Brown sr.',
-        age: 60,
-        address: 'New York No. 1 Lake Park',
-        children: [
-            {
-                key: 11,
-                name: 'John Brown',
-                age: 42,
-                address: 'New York No. 2 Lake Park',
-            },
-            {
-                key: 12,
-                name: 'John Brown jr.',
-                age: 30,
-                address: 'New York No. 3 Lake Park',
-                children: [
-                    {
-                        key: 121,
-                        name: 'Jimmy Brown',
-                        age: 16,
-                        address: 'New York No. 3 Lake Park',
-                    },
-                ],
-            },
-            {
-                key: 13,
-                name: 'Jim Green sr.',
-                age: 72,
-                address: 'London No. 1 Lake Park',
-                children: [
-                    {
-                        key: 131,
-                        name: 'Jim Green',
-                        age: 42,
-                        address: 'London No. 2 Lake Park',
-                        children: [
-                            {
-                                key: 1311,
-                                name: 'Jim Green jr.',
-                                age: 25,
-                                address: 'London No. 3 Lake Park',
-                            },
-                            {
-                                key: 1312,
-                                name: 'Jimmy Green sr.',
-                                age: 18,
-                                address: 'London No. 4 Lake Park',
-                            },
-                        ],
-                    },
-                ],
-            },
-        ],
-    },
-    {
-        key: 2,
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        children: [
-            {
-                key: 1311,
-                name: 'Jim Green jr.',
-                age: 25,
-                address: 'London No. 3 Lake Park',
-            }]
-    },
-];
-
-// rowSelection objects indicates the need for row selection
-const rowSelection: TableRowSelection<DataType> = {
-    onChange: (selectedRowKeys, selectedRows) => {
-        console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-    },
-    onSelect: (record, selected, selectedRows) => {
-        console.log(record, selected, selectedRows);
-    },
-    onSelectAll: (selected, selectedRows, changeRows) => {
-        console.log(selected, selectedRows, changeRows);
-    },
-};
-
 
 const AccountEntryExplorerTable: React.FC = () => {
-    const [checkStrictly, setCheckStrictly] = useState(false);
-
     const {data: data2, isLoading, error} = useQuery({
         queryKey: accountEntryKeys.tree_all,     // 캐싱 키
         queryFn: explorerAccountEntryStartLeaf,     // 실제 호출 함수
